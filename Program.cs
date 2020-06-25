fb2e81a4eea9cd8ca7a42615a94cec171a1425b8
benjm-ma <bahola@me.com>
2020-06-25 11:47:07 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace quizr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
