8db0e7b98a82498e09fad77542fa2923d666c4e0
benjm-ma <bahola@me.com>
2020-06-30 08:41:24 -0400

    Unit tests initial commit

---
+++ UnitTests/TestCase/CheckableAnswerTest.cs
@@ -0,0 +1,31 @@
+using System;
+using NUnit.Framework;
+
+using Quizzical.TestCase;
+
+namespace Quizzical.UnitTests
+{
+
+    [TestFixture]
+    public class CheckableAnswersTest 
+    {
+        private CheckableQuestion<int> multipleChoice;
+        private CheckableQuestion<int[]> multipleSelection;
+        private CheckableQuestion<string> shortAnswer;
+        private CheckableQuestion<bool> trueFalse;
+
+        [SetUp]
+        protected void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Should() {
+
+        } 
+    }
+
+
+
+}
