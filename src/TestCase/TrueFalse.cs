22ac0d0a41379ff03d44c46443ba72d86b06aee6
benjm-ma <bahola@me.com>
2020-06-30 08:40:31 -0400

    Refactored code, restructured project dirs

--- src/TestCase/TrueFalse.cs
+++ src/TestCase/TrueFalse.cs
@@ -12,7 +12,7 @@ namespace Quizzical.TestCase
 
         public TrueFalse (bool answer, int attempts, List<Tag> tags, 
             Measurable.Difficulty hardness, Ratio successRate, 
-            Measurable.Outcome result ): 
+            Measurable.Outcome result): 
             base (answer, attempts, tags, hardness, successRate, result) { }
 
     }
