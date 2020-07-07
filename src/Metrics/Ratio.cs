83505f6cb983715f6c4caec127458001e0a1ff92
benjm-ma <bahola@me.com>
2020-07-06 22:52:54 -0400

    Made immutable and added some basic operator overloading

--- src/Metrics/Ratio.cs
+++ src/Metrics/Ratio.cs
@@ -3,9 +3,9 @@ namespace Quizzical.Metrics
     /// <summary>
     /// A ratio.
     /// </summary>
-    public struct Ratio {
-        public int Antecedent {get;set;}
-        public int Consequent {get;set;}
+    public readonly struct Ratio {
+        public readonly int Antecedent {get;}
+        public readonly int Consequent {get;}
 
         public Ratio (int antecedent, int consequent)
         {
@@ -18,6 +18,21 @@ namespace Quizzical.Metrics
         {
             return new Ratio (0, 0);
         }
+
+        public static bool operator == (Ratio r1, Ratio r2) 
+            => r1.Antecedent == r2.Antecedent && 
+               r1.Consequent == r2.Consequent;
+
+        public static bool operator != (Ratio r1, Ratio r2)
+            => r1.Antecedent != r2.Antecedent || 
+               r1.Consequent != r2.Consequent;
+
+        public override bool Equals (object obj)
+            => obj is Ratio r1 && this.Antecedent == r1.Antecedent
+                               && this.Consequent == r1.Consequent;
+        
+        public override int GetHashCode ()
+            => (Antecedent, Consequent).GetHashCode ();
 
         public override string ToString() => $"{Antecedent}:{Consequent}";
     }
